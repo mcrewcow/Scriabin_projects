5b5e47f191d957818ae0d7054e3dad3658127685
Emil Kriukov <77118598+mcrewcow@users.noreply.github.com>
2023-05-16 23:35:45 -0400

    Update main.R

--- main.R
+++ main.R
@@ -13,3 +13,5 @@ panc_fl_ccim <- ScaleData(panc_fl_ccim) %>%
     FindNeighbors(dims = 1:10) %>%
     FindClusters(resolution = 0.2)
                              
+lyd_RGC <- SeuratWrappers::RunALRA(lyd_RGC)
+lyd_RGC__ip   <- FindAllInteractionPrograms(lyd_RGC  , iterate.threshold = 300, group.by = "background", assay = "alra", species = 'mouse')
